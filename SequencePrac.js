d5148bb8f083e8c14ea66413ac32f705747ede90
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-13 19:15:19 +0530

    Program for reading 5 Random 2 Digit values and for their sum and the average

--- SequencePrac.js
+++ SequencePrac.js
@@ -8,7 +8,20 @@ var Dice2 = Math.floor(Math.random()*7);
 console.log(Dice2);
 var Dice3 = Math.floor(Math.random()*7);
 console.log(Dice3);
-
  
+var x2 = 0;
 
+ for (var i = 1; i <= 5; i++) {
+
+    let x = Math.floor(Math.random() * (100 - 10 + 1) + 10);
+    console.log("Num " + i + " = " + x);
+    x1= x2 + x;
+     x2=x1;
+   
+    console.log("Sum of 5 random numbers is  " + x1);
+   
+}
+
+var avg = x1/5;
+console.log("Average of 5 random number is " + avg);
 

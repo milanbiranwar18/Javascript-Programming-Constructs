834bf50e99b46c6918c91e415c9842498f1a0c8d
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-13 19:05:40 +0530

    Program for Using Random to get Dice Number between 1 to 6

--- SequencePrac.js
+++ SequencePrac.js
@@ -2,3 +2,11 @@ console.log("Wellcome to the JavaScript Programing Construct Program");
 
 console.log(Math.floor(Math.random() * 10));
 
+var Dice1 = Math.floor(Math.random()*7);
+console.log(Dice1);
+var Dice2 = Math.floor(Math.random()*7);
+console.log(Dice2);
+
+ 
+
+

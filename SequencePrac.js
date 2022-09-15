1a3762578c6d41cdce80f1c7c600cffa679d833f
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-15 11:58:55 +0530

    Program for converting inch into feet

--- SequencePrac.js
+++ SequencePrac.js
@@ -1,14 +1,20 @@
 console.log("Wellcome to the JavaScript Programing Construct Program");
 
+//UC1
 console.log(Math.floor(Math.random() * 10));
 
+//UC2
 var Dice1 = Math.floor(Math.random()*7);
 console.log(Dice1);
+
+//UC3
 var Dice2 = Math.floor(Math.random()*7);
 console.log(Dice2);
 var Dice3 = Math.floor(Math.random()*7);
 console.log(Dice3);
  
+
+//UC 4
 var x2 = 0;
 
  for (var i = 1; i <= 5; i++) {
@@ -24,4 +30,14 @@ var x2 = 0;
 
 var avg = x1/5;
 console.log("Average of 5 random number is " + avg);
+
+
+//UC5 Unit Conversion (a)
+// 1 ft = 12 in 
+var Feet = 1/12;
+var inch = 42;
+console.log("42 in = " + Feet*inch + " "+"ft");   
+
+
+
 
